7af5b1a683b7db9ef6f939161ce466b0d1c4007a
James Ambrose <galaxy2198@gmail.com>
2025-11-10 14:27:15 -0600

    feat: add Invoke-TrollCommands script for sending messages to players in Docker container

---
+++ scripts/Invoke-TrollCommands.ps1
@@ -0,0 +1,30 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false)]
+    [string]
+    $dockerContainerName = 'minecraft',
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TargetPlayer
+)
+BEGIN {
+    try {
+        $dockerContainerInspect = docker inspect $dockerContainerName
+    } catch {
+        Write-Error "Unable to find container '$dockerContainerName'"
+        exit 1
+    }
+    if ($LASTEXITCODE -eq 1) {
+        Write-Error "Unable to find container '$dockerContainerName'"
+        exit 1
+    }
+}
+PROCESS {
+    if ($TargetPlayer) {
+        docker exec -it $dockerContainerName rcon-cli "/tellraw $TargetPlayer {\"text\":\"Herobrine joined the game\",\"color\":\"yellow\"}"
+    }
+    else {
+        docker exec -it $dockerContainerName rcon-cli '/tellraw @a {"text":"Herobrine joined the game","color":"yellow"}'
+    }
+}
+END {}
